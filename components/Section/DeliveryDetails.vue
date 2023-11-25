f8466e7033d75ca5932655aecefcb602d230cafa
promproexe <63090004+PromproEXE@users.noreply.github.com>
2023-11-25 23:08:00 +0700

    FIX: NO SAMPLE / GET AT FACTORY UI

--- components/Section/DeliveryDetails.vue
+++ components/Section/DeliveryDetails.vue
@@ -7,6 +7,7 @@ interface Props {
 const props = defineProps<Props>();
 const { quotation, isApproved } = toRefs(props);
 const delivery = ref<any>(quotation.value.delivery);
+const produce = ref<any>(quotation.value.produce);
 const form: any = inject('form');
 
 // COMPUTED
@@ -21,19 +22,30 @@ const totalProduct = computed(() => {
 
   return amount;
 });
+const hasSample = computed(() => produce.value.sample_type !== 'ไม่ต้อง');
+const isRecieveByMyself = computed(() => delivery.value.delivery_type === 'ลูกค้ามารับสินค้าด้วยตนเอง');
 </script>
 <template>
   <div v-once>
     <div class="grid grid-cols-2 sm:grid-cols-3 gap-5 mb-5">
-      <div>
+      <div v-if="hasSample">
         <p class="font-bold text-xl text-primary mb-3">จัดส่งตัวอย่างที่</p>
         <p class="font-medium">{{ delivery.sample_full_name }}</p>
         <p class="word-break">{{ delivery.sample_address }}</p>
       </div>
-      <div>
+      <div :class="{ 'col-span-2': !hasSample }">
         <p class="font-bold text-xl mb-3 text-primary">จัดส่งสินค้าที่</p>
-        <p class="font-medium">{{ delivery.full_name }}</p>
-        <p class="word-break">{{ delivery.address }}</p>
+        <div v-if="isRecieveByMyself">
+          <p class="word-break">โรงงานสมศรีมีเสื้อ</p>
+          <p class="font-medium mb-3">รับสินค้าโดยคุณ: {{ delivery.full_name }}</p>
+          <NuxtLink to="https://maps.app.goo.gl/CqVmTeQdjkc4CBRDA" target="_blank">
+            <button class="btn btn-sm btn-primary"><IconCSS name="material-symbols:location-on-outline-rounded" size="1rem"></IconCSS>ดูแผนที่</button>
+          </NuxtLink>
+        </div>
+        <div v-else>
+          <p class="font-medium">{{ delivery.full_name }}</p>
+          <p class="word-break">{{ delivery.address }}</p>
+        </div>
       </div>
       <div>
         <p class="font-bold text-primary">จำนวน</p>
