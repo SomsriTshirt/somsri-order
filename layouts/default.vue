7253afd4a392eb93eb1c1d895bf13289a6718aa0
promproexe <63090004+PromproEXE@users.noreply.github.com>
2023-11-25 22:11:43 +0700

    FIX: DEFAULT LAYOUT

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import Navbar from '@/components/Component/NavbarComponent.vue'
+import Navbar from '@/components/Component/NavbarComponent.vue';
 </script>
 <template>
-    <div>
-        <Navbar class="mb-5"></Navbar>
-        <div class="mb-16">
-            <slot></slot>
-        </div>
+  <div>
+    <Navbar class="mb-5"></Navbar>
+    <div class="mb-16">
+      <div class="container mx-auto px-6 pt-6">
+        <slot></slot>
+      </div>
     </div>
+  </div>
 </template>
