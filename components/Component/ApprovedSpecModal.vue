22a8c54558447544254fc34b76eae65c9a52d7ab
suphakamonLak <165622318+suphakamonLak@users.noreply.github.com>
2024-05-30 15:32:14 +0700

    ADD: FEATURE

--- components/Component/ApprovedSpecModal.vue
+++ components/Component/ApprovedSpecModal.vue
@@ -44,7 +44,7 @@ async function approvedSpec() {
 </script>
 <template>
   <dialog id="approved-spec-modal" class="modal">
-    <div class="modal-box">
+    <div class="modal-box w-11/12 max-w-3xl">
       <div class="flex justify-end mb-3">
         <form method="dialog">
           <button class="btn btn-sm btn-ghost btn-circle">
@@ -52,16 +52,46 @@ async function approvedSpec() {
           </button>
         </form>
       </div>
-      <div class="mb-5">
-        <h3 class="text-center text-3xl font-bold mb-3">คุณต้องการอนุมัติใบสเปคนี้หรือไม่</h3>
-        <p class="text-center text-warning">เมื่อคุณกดอนุมัติแล้ว จะไม่สามารถขอแก้ไขข้อมูลในใบสเปคได้ <br />โปรดเช็คให้มั่นใจก่อนกดยืนยัน</p>
+      <div class="border border-gray-400 dark:border-neutral-600 rounded-xl shadow p-4 mb-3">
+        <p class="text-center text-3xl font-bold text-primary">เงื่อนไขการแก้ไขตัวอย่าง</p>
+        <div class="my-2">
+          <p>1. สินค้าจะผลิตตาม ArtWork ที่ถูกยืนยันเท่านั้น กรุณาตรวจสอบรายละเอียดให้เรียบร้อยก่อนทำการอนุมัติ</p>
+          <p>2. หลังอนุมัติใบสเปคแล้ว จะไม่สามารถเปลี่ยนลายสกรีน/ปัก เนื้อผ้า สีผ้า รูปทรง และวัตถุดิบได้</p>
+          <p>3. หลังได้รับตัวอย่างงาน รบกวนอนุมัติงานผลิตภายใน 24 ชั่วโมง(ตัวอย่างจะเป็นการถ่ายรูปเท่านั้น หากต้องการให้ส่งตัวอย่างจริง จะมีค่าจัดส่งเพิ่มเติม 60 บาท)</p>
+          <p>4. หากงานตัวอย่างมีการแก้ไขหรือเปลี่ยนแปลง กำหนดการผลิตสินค้าจะถูกเลื่อนออกไปจนกว่าตัวอย่างจะถูกอนุมัติและจะแจ้งกำหนดการเสร็จสิ้นอีกครั้ง</p>
+        </div>
+        <div class="grid grid-cols-1 gap-4 my-2 md:grid-cols-2 gap-3">
+          <div class="px-10">
+            <h5 class="text-center font-bold text-primary text-xl">สิ่งที่แก้ไขได้</h5>
+              <ul class="list-disc">
+                <li>สีสรีน</li>
+                <li>ตำแหน่งการปัก/สกรีน</li>
+                <li>ปัญหาที่เกิดขึ้นจากความผิดพลาดของโรงงาน</li>
+              </ul>
+          </div>
+          <div class="px-10">
+            <h5 class="text-center font-bold text-primary text-xl">สิ่งที่แก้ไขไม่ได้</h5>
+              <ul class="list-disc"> 
+                <li>เนื้อผ้า/สีผ้า</li>
+                <li>Pattern/ขนาดเสื้อ</li>
+                <li>วัตถุดิบประกอบเสื้อ</li>
+                <li>เปลี่ยนแบบสกรีน/ปัก</li>
+                <li>ขนาดของลาย</li>
+                <li>เปลี่ยนสีงานพิมพ์</li>
+              </ul>
+          </div>
+        </div>
+        <div>
+            <p><b>หมายเหตุ: </b>เนื้อผ้ามีโอกาสหดหลังการซักครั้งแรก 1-2 นิ้ว รบกวนเผื่อขนาดอกก่อนสั่งผลิตงานพิมพ์ Sublimation สีพิมพ์จะต่างจากสีไฟล์งานบวกลบ 10-15% ขึ้นอยู่กับเครื่องพิมพ์</p>
+            <p class="text-primary font-bold">หลังอนุมัติใบสเปคแล้วจะไม่สามารถแก้ไขแบบได้ รบกวนตรวจสอบใบสเปคให้เรียบร้อยก่อนอนุมัติ</p>
+        </div>
       </div>
 
       <form method="dialog">
-        <div class="grid grid-cols-2 gap-5">
+        <div class="grid grid-cols-2 gap-4">
           <!-- if there is a button in form, it will close the modal -->
-          <button class="btn btn-ghost border border-neutral-700 text-lg">ยกเลิก</button>
-          <button class="btn btn-success text-lg" @click="approvedSpec()">ยืนยัน</button>
+          <button class="btn btn-ghost border border-neutral-700 text-base">ยกเลิก</button>
+          <button class="btn btn-success text-base" @click="approvedSpec()">ฉันอ่านและยอมรับเงื่อนไขข้างต้น</button>
         </div>
       </form>
     </div>
