53c44ad9d15c9cb5b7b39c393a966d44a2b0defd
promproexe <63090004+PromproEXE@users.noreply.github.com>
2024-10-06 01:58:09 +0700

    CONVERT: TO USE SHADCN

--- components/Component/StatusComponent.vue
+++ components/Component/StatusComponent.vue
@@ -1,105 +1,125 @@
 <script setup lang="ts">
-import MoreStatusDetails from '@/components/Component/MoreStatusDetails.vue';
+import type { StatusList } from '~/types/Global';
+import type { SpecSheet } from '~/types/SpecSheet';
+import type { WorkOrder } from '~/types/WorkOrder';
+import { StepperItem, Stepper, StepperSeparator } from '../ui/stepper';
+
+// LAZY LOAD
+const MoreStatusDetails = defineAsyncComponent(() => import('@/components/Component/MoreStatusDetails.vue'));
+
 // PROPS
 interface Props {
-  specSheet: any;
+    specSheet: SpecSheet;
 }
 const props = defineProps<Props>();
 const { specSheet } = toRefs(props);
-const stepList = ref<any[]>([]);
-const stepData = ref<any>({});
-const finishAll = ref<any>({});
 
-defineEmits(['reload-spec-sheet']);
+// EMIT
+type Emit = {
+    'reload-spec-sheet': [];
+};
+defineEmits<Emit>();
 
 // VARIABLE
-const isLoading = ref<boolean>(true);
+const stepList = ref<StatusList[]>([]);
+const currentStatus = ref<string>('');
+const finishAll = ref<boolean>(false);
+const moreStatusDetailOpenState = ref<boolean>(false);
 
-function getStepData() {
-  let isFinishAll = true;
-  if (specSheet.value.workOrders[0]) {
-    for (const step of specSheet.value.workOrders[0].stepList) {
-      if (!specSheet.value.workOrders[0].stepData[step.id].done) {
-        stepData.value = `${step.label}`;
-        isFinishAll = false;
-        break;
-      }
+// COMPUTED
+const haveWorkOrder = computed<boolean>(() => specSheet.value.workOrders.length > 0);
+const workOrder = computed<WorkOrder | null>(() => (haveWorkOrder.value ? specSheet.value.workOrders[0] : null));
+const progressIcon = computed<string>(() => (finishAll.value ? 'material-symbols:check-small' : 'material-symbols:cut-rounded'));
+
+// SET VARIABLE
+finishAll.value = !!workOrder.value?.isFinish;
+currentStatus.value = getCurrentStatus();
+stepList.value = getStepList();
+
+// FUNCTION
+function getCurrentStatus(): string {
+    if (specSheet.value.workOrders.length === 0) return 'ไม่ระบุ';
+
+    if (!workOrder.value) return 'ไม่ระบุ';
+
+    for (const step of workOrder.value.stepList) {
+        if (!workOrder.value.stepData[step.id].done) {
+            return `รอ${step.label}`;
+        }
     }
-    if (isFinishAll) {
-      const lastIndex = specSheet.value.workOrders[0].stepList.length - 1;
-      const lastStep = specSheet.value.workOrders[0].stepList[lastIndex];
-      finishAll.value = lastStep.label;
-    }
-  }
+
+    // GET LAST STEP
+    const lastIndex = workOrder.value.stepList.length - 1;
+    const lastStep = workOrder.value.stepList[lastIndex];
+    return `${lastStep.label}แล้ว`;
 }
 
-function getStepList() {
-  if (specSheet.value.workOrders[0]) {
-    stepList.value = specSheet.value.workOrders[0].stepList.slice(1);
-  }
+function getStepList(): StatusList[] {
+    return workOrder.value?.stepList.slice(1) || [];
 }
 
 function isPassStatus(step: string) {
-  const stId = ref<any>({});
-  for (const steps of specSheet.value.workOrders[0].stepList) {
-    if (!specSheet.value.workOrders[0].stepData[steps.id].done) {
-      stId.value = `${steps.id}`;
-      break;
+    const stId = ref<any>({});
+    for (const steps of specSheet.value.workOrders[0].stepList) {
+        if (!specSheet.value.workOrders[0].stepData[steps.id].done) {
+            stId.value = `${steps.id}`;
+            break;
+        }
     }
-  }
-  if (specSheet.value.workOrders[0].stepData[step].done || step === stId.value) {
-    return true;
-  } else {
-    return false;
-  }
+    if (specSheet.value.workOrders[0].stepData[step].done || step === stId.value) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
-// ON MOUNTED
-onMounted(() => {
-  isLoading.value = false;
-  getStepData();
-  getStepList();
-});
 </script>
 <template>
-  <div v-if="!isLoading">
-    <div v-if="stepData.length > 0" class="mb-5">
-      <div class="rounded-xl p-5 shadow mb-5 bg-secondary">
-        <div class="flex">
-          <div class="flex flex-col items-center">
-            <div class="w-30 h-30 rounded-full border-4 border-orange-800 dark:border-orange-500 border-dashed text-orange-800 dark:text-orange-600 flex justify-center items-center p-5 mr-3">
-              <IconCSS name="material-symbols:cut-rounded" size="4rem"></IconCSS>
+    <div>
+        <template v-if="haveWorkOrder">
+            <div class="rounded-xl p-5 shadow mb-5" :class="[finishAll ? 'bg-green-500' : 'bg-orange-300']">
+                <div class="flex">
+                    <div class="flex flex-col items-center">
+                        <div
+                            class="w-30 h-30 rounded-full border-4 border-orange-800 dark:border-orange-500 border-dashed text-orange-800 dark:text-orange-600 flex justify-center items-center p-5 mr-3"
+                        >
+                            <IconCSS :name="progressIcon" size="5rem" />
+                        </div>
+                    </div>
+                    <div>
+                        <div class="mb-3">
+                            <p class="text-white dark:text-blacktext-2xl sm:text-4xl font-bold">สถานะการผลิต</p>
+                            <p class="text-black text-xl font-bold">{{ finishAll ? 'การผลิตเสร็จแล้ว' : currentStatus }}</p>
+                        </div>
+                        <div class="flex flex-col sm:flex-row">
+                            <!-- <Button @click="moreStatusDetailOpenState = true">ดูสถานะแบบละเอียด</Button> -->
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div>
-            <div class="mb-3">
-              <p class="text-white dark:text-blacktext-2xl sm:text-4xl font-bold">สถานะการผลิต</p>
-              <p class="text-black text-xl font-bold">รอ{{ stepData }}</p>
+            <div class="bg-white dark:bg-stone-800 rounded-xl border border-stone-200 dark:border-stone-600 p-5 shadow">
+                <p class="text-xl font-bold mb-5 text-primary-700 dark:text-primary-400">สถานะการผลิตโดยรวม</p>
+                <Stepper class="justify-between items-start">
+                    <StepperItem v-for="(step, stepI) in stepList" :key="step.label" class="w-full" :step="stepI + 1" :completed="isPassStatus(step.id)">
+                        <div class="flex flex-col items-center w-full">
+                            <div class="w-5 h-5 rounded-full" :class="[isPassStatus(step.id) ? 'bg-green-500' : 'bg-stone-300 dark:bg-stone-600']"></div>
+                            <p class="dark:text-stone-300 text-sm text-center mt-1">{{ step.label }}</p>
+                        </div>
+
+                        <StepperSeparator v-if="stepI !== stepList.length - 1" class="w-full h-px" />
+                    </StepperItem>
+                </Stepper>
             </div>
-            <div class="flex flex-col sm:flex-row">
-              <button class="btn btn-sm btn-primary" @click="openModal('more-status-details-modal')">ดูสถานะแบบละเอียด</button>
-            </div>
-          </div>
+        </template>
+
+        <div v-else class="rounded-xl p-5 shadow mb-5 bg-orange-300">
+            <p class="text-white text-center dark:text-blacktext-2xl sm:text-4xl font-bold">ไม่พบข้อมูลการผลิต</p>
         </div>
-      </div>
-      <div class="bg-white dark:bg-neutral-800 rounded-xl p-5 shadow">
-        <p class="text-xl font-bold mb-5 text-primary">สถานะการผลิตโดยรวม</p>
-        <ul class="steps w-full">
-          <li v-for="step in stepList" :key="useGenVueKey(step)" class="step" :class="{ 'step-primary': isPassStatus(step.id) }">{{ step.label }}</li>
-        </ul>
-      </div>
+        <div class="mt-5">
+            <slot />
+        </div>
+        <!-- MODAL -->
+        <Suspense v-if="moreStatusDetailOpenState">
+            <MoreStatusDetails v-model:open="moreStatusDetailOpenState" :spec-sheet="specSheet" :step-list="stepList" />
+        </Suspense>
     </div>
-    <div v-else-if="finishAll.length > 0" class="rounded-xl p-5 shadow mb-5 bg-green-500 flex">
-      <div class="w-30 h-30 rounded-full border-4 border-white border-dashed text-white flex justify-center items-center mr-3">
-        <IconCSS name="material-symbols:check-small" size="5rem"></IconCSS>
-      </div>
-      <p class="text-white p-5 dark:text-blacktext-2xl sm:text-4xl font-bold">การผลิตเสร็จแล้ว</p>
-    </div>
-    <div v-else class="rounded-xl p-5 shadow mb-5 bg-secondary">
-      <p class="text-white text-center dark:text-blacktext-2xl sm:text-4xl font-bold">ไม่พบข้อมูลการผลิต</p>
-    </div>
-    <slot></slot>
-    <!-- MODAL -->
-    <MoreStatusDetails :spec-sheet="specSheet" :step-list="stepList" :step-data="stepData"></MoreStatusDetails>
-  </div>
 </template>
