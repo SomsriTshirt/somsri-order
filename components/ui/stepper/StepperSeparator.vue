ce7304d08ca3611fe3e53e0e6d3980bf1155cb8b
promproexe <63090004+PromproEXE@users.noreply.github.com>
2024-10-06 01:57:47 +0700

    FIX: STEPPER SEPERATOR COLOR

--- components/ui/stepper/StepperSeparator.vue
+++ components/ui/stepper/StepperSeparator.vue
@@ -1,31 +1,33 @@
 <script lang="ts" setup>
-import { type HTMLAttributes, computed } from 'vue'
-import type { StepperSeparatorProps } from 'radix-vue'
-import { StepperSeparator, useForwardProps } from 'radix-vue'
+import { type HTMLAttributes, computed } from 'vue';
+import type { StepperSeparatorProps } from 'radix-vue';
+import { StepperSeparator, useForwardProps } from 'radix-vue';
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils';
 
-const props = defineProps<StepperSeparatorProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<StepperSeparatorProps & { class?: HTMLAttributes['class'] }>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+    const { class: _, ...delegated } = props;
 
-  return delegated
-})
+    return delegated;
+});
 
-const forwarded = useForwardProps(delegatedProps)
+const forwarded = useForwardProps(delegatedProps);
 </script>
 
 <template>
-  <StepperSeparator
-    v-bind="forwarded"
-    :class="cn(
-      'bg-stone-100 dark:bg-stone-800',
-      // Disabled
-      'group-data-[disabled]:bg-stone-100 group-data-[disabled]:opacity-50 dark:group-data-[disabled]:bg-stone-800',
-      // Completed
-      'group-data-[state=completed]:bg-stone-900 dark:group-data-[state=completed]:bg-stone-50',
-      props.class,
-    )"
-  />
+    <StepperSeparator
+        v-bind="forwarded"
+        :class="
+            cn(
+                'bg-stone-100 dark:bg-stone-600',
+                // Disabled
+                'group-data-[disabled]:bg-stone-100 group-data-[disabled]:opacity-50 dark:group-data-[disabled]:bg-stone-700',
+                // Completed
+                'group-data-[state=completed]:bg-stone-900 dark:group-data-[state=completed]:bg-stone-50',
+                props.class,
+            )
+        "
+    />
 </template>
