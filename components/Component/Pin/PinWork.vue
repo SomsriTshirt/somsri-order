6e373df60ee52a6f85055ee83c3e98cecf15620e
promproexe <63090004+PromproEXE@users.noreply.github.com>
2024-10-06 00:25:40 +0700

    CONVERT: ETC WORK TO SHADCN

--- components/Component/Pin/PinWork.vue
+++ components/Component/Pin/PinWork.vue
@@ -1,46 +1,53 @@
 <script setup lang="ts">
 // PROPS
 interface Props {
-  pin: any;
-  index: number;
+    pin: any;
+    index: number;
 }
 const props = defineProps<Props>();
 const { pin, index } = toRefs(props);
+
+const pinDetails = [
+    {
+        label: 'ตำแหน่งงานปัก',
+        value: pin.value.position,
+    },
+    {
+        label: 'ขนาดงานปัก',
+        value: pin.value.size,
+    },
+    {
+        label: 'ระยะห่างจากคอเสื้อถึงตัวงาน',
+        value: pin.value.spaceFromCollar,
+    },
+    {
+        label: 'ชนิดงานปัก',
+        value: pin.value.type,
+    },
+    {
+        label: 'สีงานปัก',
+        value: pin.value.color,
+    },
+    {
+        label: 'ไฟล์เสื้อ',
+        value: pin.value.file,
+    },
+    {
+        label: 'ลักษณะงานปักเพิ่มเติม',
+        value: pin.value.note || '-',
+    },
+];
 </script>
 <template>
-  <div>
-    <h3 class="text-2xl text-white dark:text-black text-center font-medium bg-primary rounded-t-xl p-3">จุดปักที่ {{ index + 1 }}</h3>
-    <table class="table border border-collapse border-neutral-300 dark:border-neutral-700 text-lg bg-white dark:bg-base-100 shadow">
-      <tbody>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ตำแหน่งงานปัก</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.position }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ขนาดงานปัก</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.size }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ระยะห่างจากคอเสื้อถึงตัวงาน</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.spaceFromCollar }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ชนิดงานปัก</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.type }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">สีงานปัก</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.color }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ไฟล์เสื้อ</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.file }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ลักษณะงานปักเพิ่มเติม</td>
-          <td class="capitalize border dark:border-neutral-700">{{ pin.note || '-' }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div>
+        <h3 class="text-2xl text-white dark:text-black text-center font-medium bg-primary-700 dark:bg-primary-400 rounded-t-xl p-3">จุดปักที่ {{ index + 1 }}</h3>
+        <Table>
+            <TableBody class="bg-stone-100 dark:bg-stone-800">
+                <TableRow v-for="detail in pinDetails" :key="detail.label" class="text-lg dark:text-white">
+                    <TableCell class="font-medium">{{ detail.label }}</TableCell>
+                    <TableCell>{{ detail.value || '-' }}</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </div>
 </template>
--- components/Component/Print/PrintWork.vue
+++ components/Component/Print/PrintWork.vue
@@ -1,46 +1,53 @@
 <script setup lang="ts">
 // PROPS
 interface Props {
-  print: any;
-  index: number;
+    print: any;
+    index: number;
 }
 const props = defineProps<Props>();
 const { print, index } = toRefs(props);
+
+const printDetails = [
+    {
+        label: 'ตำแหน่งงานพิมพ์',
+        value: print.value.position,
+    },
+    {
+        label: 'ขนาดงานพิมพ์',
+        value: print.value.size,
+    },
+    {
+        label: 'ระยะห่างจากคอเสื้อถึงตัวงาน',
+        value: print.value.spaceFromCollar,
+    },
+    {
+        label: 'ชนิดงานพิมพ์',
+        value: print.value.type,
+    },
+    {
+        label: 'สีงานพิมพ์',
+        value: print.value.color,
+    },
+    {
+        label: 'ไฟล์เสื้อ',
+        value: print.value.file,
+    },
+    {
+        label: 'ลักษณะงานพิมพ์เพิ่มเติม',
+        value: print.value.note || '-',
+    },
+];
 </script>
 <template>
-  <div>
-    <h3 class="text-2xl text-white dark:text-black text-center font-medium bg-primary rounded-t-xl p-3">จุดพิมพ์ที่ {{ index + 1 }}</h3>
-    <table class="table border border-collapse border-neutral-300 dark:border-neutral-700 text-lg bg-white dark:bg-base-100 shadow">
-      <tbody>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ตำแหน่งงานพิมพ์</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.position }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ขนาดงานพิมพ์</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.size }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ระยะห่างจากคอเสื้อถึงตัวงาน</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.spaceFromCollar }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ชนิดงานพิมพ์</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.type }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">สีงานพิมพ์</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.color }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ไฟล์เสื้อ</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.file }}</td>
-        </tr>
-        <tr>
-          <td class="font-medium border dark:border-neutral-700">ลักษณะงานพิมพ์เพิ่มเติม</td>
-          <td class="capitalize border dark:border-neutral-700">{{ print.note || '-' }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div>
+        <h3 class="text-2xl text-white dark:text-black text-center font-medium bg-primary-700 dark:bg-primary-400 rounded-t-xl p-3">จุดพิมพ์ที่ {{ index + 1 }}</h3>
+        <Table>
+            <TableBody class="bg-stone-100 dark:bg-stone-800">
+                <TableRow v-for="detail in printDetails" :key="detail.label" class="text-lg dark:text-white">
+                    <TableCell class="font-medium">{{ detail.label }}</TableCell>
+                    <TableCell>{{ detail.value || '-' }}</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </div>
 </template>
